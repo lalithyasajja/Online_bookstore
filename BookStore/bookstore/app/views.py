01228886a1e8fa6f1107f98161bf87a90f2093c9
psc215 <chaitanya.sai215@gmail.com>
2023-07-09 23:54:22 -0600

    Removed comments and added book details page

--- BookStore/bookstore/app/views.py
+++ BookStore/bookstore/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.db.models import Count
 from . models import Book
@@ -15,6 +15,7 @@ class CategoryView(View):
     def get(self, request, val):
         book = Book.objects.filter(category=val)
         title = Book.objects.filter(category=val).values('title')
+        category_name = Book.objects.filter(category=val).values('category')
         return render(request, "app/category.html", locals())
 
 class SearchView(View):
@@ -40,3 +41,8 @@ class ProfileView(View):
 class ChangePwdView(View):
     def get(self, request):
         return render(request, "app/changePwd.html", locals())
+
+class BookDetailsView(View):
+    def get(self, request, book_isbn):
+        book = get_object_or_404(Book, ISBN=book_isbn)
+        return render(request, "app/bookDetails.html", locals())
